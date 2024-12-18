57e98a317e7f2abbc9fa4f6d84d3ebde425f4ad7
RKest <max.ind@o2.pl>
2024-12-18 11:57:04 +0100

    Removed one useless file

--- Program.fs
+++ Program.fs
@@ -251,6 +251,39 @@ module View =
     let notePreviewHomeId = "note-preview-home"
     let tagEditDropdownId = "tags-edit-dropdown"
 
+    let javascriptSrc =
+        """
+        const selectImage = async () => {
+            let sel = window.getSelection();
+            let range = sel.getRangeAt(0);
+            let clone = range.cloneContents();
+            if (!clone.firstElementChild) {
+                alert('Select an image first');
+                return;
+            }
+            let body = clone.firstElementChild.getAttribute('src');
+            if (!body) {
+                alert('Select an image first');
+                return;
+            }
+            let method = 'POST';
+            let text = await fetch("/ocr", { body, method }).then((res) => res.text());
+            range.insertNode(document.createTextNode(text));
+        };
+
+        htmx.defineExtension('path-params', {
+            onEvent: function(name, evt) {
+                if (name === "htmx:configRequest") {
+                    evt.detail.path = evt.detail.path.replace(/{{([A-Za-z0-9_]+)}}/g, function (_, param) {
+                        let val = evt.detail.parameters[param];
+                        delete evt.detail.parameters[param]; // don't pass in query string since already in path
+                        return val;
+                  })
+                }
+            }
+        });
+        """
+
     let notifBar msg =
         Elem.div [ Attr.height "2rem"; Attr.id notifBarId ] [ Text.raw msg ]
 
@@ -262,7 +295,7 @@ module View =
                   [ Elem.title [] [ Text.raw "Note" ]
                     Elem.script [ Attr.src htmxSrc ] []
                     Elem.script [ Attr.src jsonEncSrc ] []
-                    Elem.script [ Attr.src "/script.js" ] [] ]
+                    Elem.script [] [ Text.raw javascriptSrc ] ]
               Elem.body [] [ content; notifBar "" ] ]
 
 
@@ -519,8 +552,6 @@ module Handlers =
 
 
 webHost [||] {
-    use_static_files
-
     endpoints
         [ Handlers.home' |> get "/"
           Handlers.notePreviews |> get "/notePreviews"
